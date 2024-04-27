db0bb10cc5563ce0fd85c8fc630bafed7739bd26
Ajay Kaloty <kalotya99@gmail.com>
2024-04-27 14:28:49 -0700

    Added switch cases

--- script.js
+++ script.js
@@ -46,6 +46,29 @@ function mainMenu() {
             name: 'mainMenu',
             message: 'What would you like to do?',
             choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update Employee Role', 'Exit']
-        }
+        },
     ])
+    .then((answers) => {
+        switch (answers.mainMenu) {
+            case 'View All Departments': viewAllDepartments();
+                break;
+            case 'View All Roles': viewAllRoles();
+                break;
+            case 'View All Employees': viewAllEmployees();
+                break;
+            case 'Add A Department': addDepartment();
+                break;
+            case 'Add A Role': addRole();
+                break;
+            case 'Add An Employee': addEmployee();
+                break;
+            case 'Update Employee Role': updateEmployeeRole();
+                break;
+            case 'Exit': console.log('Goodbye!');
+                connection.end();
+                break;
+            default: console.log('Invalid Choice!');
+                mainMenu();
+        }
+    })
 };
